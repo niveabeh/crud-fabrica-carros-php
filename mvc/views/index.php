de63cd2a142b0f4dba1466aa6072b47c0402b7c5
niveabeh <niveasouza86@gmail.com>
2026-03-13 13:09:09 -0300

    adicionando na views a tabela com os dados

--- mvc/views/index.php
+++ mvc/views/index.php
@@ -1,1 +1,31 @@
-<!-- Listagem de serviços -->
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Veículos</h1>
+    <div class="content">
+        <table class="table">
+            <thead>
+                <tr>ID</tr>
+                <tr>Modelo</tr>
+                <tr>Cor</tr>
+            </thead>
+            <tbody>
+                <?php
+                foreach($resultData as $data): ?>
+                    <tr>
+                    <td><?=  $data['id'] ?></td>
+                    <td><?=  $data['modelo'] ?></td>
+                    <td><?=  $data['cor'] ?></td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
+    
+</body>
+</html>
