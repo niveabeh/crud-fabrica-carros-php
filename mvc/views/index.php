b91df24ca4b4e4fca92d410aa33c2aeee8922adc
niveabeh <niveasouza86@gmail.com>
2026-03-13 13:28:32 -0300

    adicionando style

--- mvc/views/index.php
+++ mvc/views/index.php
@@ -1,31 +1,38 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/crud-fabrica-carros-php/mvc/views/css/style.css">
     <title>Document</title>
 </head>
+
 <body>
     <h1>Veículos</h1>
     <div class="content">
         <table class="table">
             <thead>
-                <tr>ID</tr>
-                <tr>Modelo</tr>
-                <tr>Cor</tr>
+                <tr>
+                    <th>ID</th>
+                    <th>Modelo</th>
+                    <th>Cor</th>
+                </tr>
             </thead>
             <tbody>
                 <?php
-                foreach($resultData as $data): ?>
+                
+                foreach ($resultData as $data): ?>
                     <tr>
-                    <td><?=  $data['id'] ?></td>
-                    <td><?=  $data['modelo'] ?></td>
-                    <td><?=  $data['cor'] ?></td>
+                        <td><?= $data['id'] ?></td>
+                        <td><?= $data['modelo'] ?></td>
+                        <td><?= $data['cor'] ?></td>
                     </tr>
                 <?php endforeach; ?>
             </tbody>
         </table>
     </div>
-    
+
 </body>
+
 </html>
