65513bd8d11b5a4d492de6f3805f47d406136863
niveabeh <niveasouza86@gmail.com>
2026-03-13 11:37:51 -0300

    conectando o banco de dados

--- mvc/configuration/connect.php
+++ mvc/configuration/connect.php
@@ -1,1 +1,25 @@
 <!-- Conexão com o banco de dados -->
+
+<?php
+    define('HOST', 'localhost');
+    define('DATABASENAME', 'crud_fabrica_carros_php');
+    define('USER', 'root');
+    define('PASSWORD', ''); //Não tem senha
+
+    class connect{
+        protected $connection;
+
+        function __construct()
+        {
+            $this->connectDatabase();
+        }
+        function connectDatabase(){
+            try{
+                $this->connection = new PDO('mysql:host='.HOST.';dbname='.DATABASENAME, USER, PASSWORD);
+            }catch(PDOException $e){
+                echo "Erro na conexão: " . $e->getMessage();
+            }
+        }
+    }
+
+?>
