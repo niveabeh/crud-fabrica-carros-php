26f466a40b30cc1dc053e6a0233f19744bfcebd7
niveabeh <niveasouza86@gmail.com>
2026-03-17 16:05:43 -0300

    fazendo uma página para mensagem de salvar (ainda nao sei como vai funcionar)

--- Fabrica_Carros/Views/sucessoFabricacao.php
+++ Fabrica_Carros/Views/sucessoFabricacao.php
@@ -1,38 +1,41 @@
-
-
 <!DOCTYPE html>
 <html lang="pt-br">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/crud-fabrica-carros-php/mvc/views/css/style.css">
+    <link rel="stylesheet" href="/crud-fabrica-carros-php/Fabrica_Carros/Views/assets/css/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" defer></script>
-    <script src="/crud-fabrica-carros-php/mvc/views/js/main.js" defer> </script>
-    <title>Fabricar</title>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous" defer></script>
+    <title>Home</title>
 </head>
 
 <body>
-    <div class="container-pai">
-
-        <div class="container-esquerdo">
-
-            <div class="caixa-card">
-                <h3>Sucesso na Fabricação</h3>
-                <p class="sucesso">
-                    Os veículos da categoria <strong><?= $tipo ?></strong> foram fabricados com sucesso.
-                </p>
-                <div class="caixa-btn">
-                    <a href="../View/index.html" class="btn-segundo">Voltar ao menu</a>
+    <div class="container">
+        <div class="row min-vh-100 d-flex  align-items-center justify-content-start">
+            <div class="col-12 col-lg-6">
+                <!-- fotos -->
+                <div class="container mb-4">
+                    <div class="row">
+                        <div class="col-2">
+                            <img src="/crud-fabrica-carros-php/Fabrica_Carros/Views/assets/img/nivea.jpg" class="img-fluid w-25 rounded-circle shadow border border-white " alt="Imagem perfil Nívea">
+                            <img src="/crud-fabrica-carros-php/Fabrica_Carros/Views/assets/img/emilly.png" class="img-fluid w-25 rounded-circle shadow border border-white " alt="Imagem perfil Nívea">
+                        </div>
+                    </div>
                 </div>
-
+                <div class="mb-2">
+                    <h2 class="h2 text-secondary">Home</h2>
+                    <h1 class="text-roxo h1 fw-bold  ">Fábrica de Veículos</h1>
+                </div>
+                <div class="rounded-4 shadow bg-light p-2 mt-4 ">
+                    <h3 class="text-center text-secondary">Veículos Salvo</h3>
+                    <p>Os dados foram salvos com <span>Sucesso!</span></p>
+                </div>
+                <p class="text-secondary mt-4"><small> Este programa faz parte da atividade final da disciplina de <span class="text-roxo fw-semibold">Programação Orientada a Objetos em PHP</span>. O projeto apresenta uma simulação dinâmica de uma fábrica de veículos, permitindo criar, listar, editar e excluir veículos</small></p>
+                <div class="col-12 col-lg-6">
+                </div>
             </div>
-
         </div>
-
-        <div class="container-direito"></div>
-
     </div>
 </body>
 
