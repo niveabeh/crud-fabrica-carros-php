a7e79d7901e567b4587b1234946bf3e1bd5269e3
niveabeh <niveasouza86@gmail.com>
2026-03-17 15:59:59 -0300

    link do css;criando variaveis de cor;estilizando home;adicionando identidade

--- Fabrica_Carros/Views/home.php
+++ Fabrica_Carros/Views/home.php
@@ -4,41 +4,43 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="View/assets/css/style.css">
-    <title>Menu-Fabrica de Veiculos</title>
+    <link rel="stylesheet" href="/crud-fabrica-carros-php/Fabrica_Carros/Views/assets/css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous" defer></script>
+    <title>Home</title>
 </head>
 
 <body>
-    <div class="container-pai">
-
-        <div class="container-esquerdo-menu">
-            <div class="fotos-integrantes">
-                <img src="../assets/img/emilly.png" alt="Emmilly luz" class="fotos-inte">
-                <img src="../assets/img/nivea.jpg" alt="Nivea souza" class="fotos-inte">
-                <img src="../assets/img/ana.png" alt="Ana" class="fotos-inte">
-            </div>
-            <div class="titulos">
-                <h2 class="subtitulo-principal">monte o seu veículo</h2>
-                <h1 class="texto-h1">Fábrica de Veículos</h1>
-                
-            </div>
-
-            <div class="caixa-card">
-                <h3>Menu Princial</h3>
-                <div class="container-menu">
-
-                        <div class="caixa-btn">
-                            <a href="index.php?a=fabricar"  class="btn-primeiro">Fabricar Veículo</a>
-                            <a href="index.php?a=informacoes"  class="btn-primeiro">Informações</a>
+    <div class="container">
+        <div class="row min-vh-100 d-flex  align-items-center justify-content-start">
+            <div class="col-12 col-lg-6">
+                <!-- fotos -->
+                <div class="container mb-4">
+                    <div class="row">
+                        <div class="col-2">
+                            <img src="/crud-fabrica-carros-php/Fabrica_Carros/Views/assets/img/nivea.jpg" class="img-fluid w-25 rounded-circle shadow border border-white " alt="Imagem perfil Nívea">
+                            <img src="/crud-fabrica-carros-php/Fabrica_Carros/Views/assets/img/emilly.png" class="img-fluid w-25 rounded-circle shadow border border-white " alt="Imagem perfil Nívea">
+                        </div>
+                    </div>
+                </div>
+                <div class="mb-2">
+                    <h2 class="h2 text-secondary">Home</h2>
+                    <h1 class="text-roxo h1 fw-bold  ">Fábrica de Veículos</h1>
+                </div>
+                <div class="rounded-4 shadow bg-light p-2 mt-4 " >
+                    <h3 class="text-center text-secondary">Menu Principal</h3>
+                    <div class="container-menu">
+                        <div class="container-fluid d-flex flex-column gap-2 justify-content-center" style="min-height:220px;">
+                            <a href="index.php?a=fabricar" class="btn btn-primeiro fw-semibold shadow ">Fabricar Veículo</a>
+                            <a href="index.php?a=informacoes" class="btn btn-outline-primeiro fw-semibold shadow">Informações</a>
                         </div>
 
+                    </div>
+                </div>
+                <p class="text-secondary mt-4"><small> Este programa faz parte da atividade final da disciplina de <span class="text-roxo fw-semibold">Programação Orientada a Objetos em PHP</span>. O projeto apresenta uma simulação dinâmica de uma fábrica de veículos, permitindo criar, listar, editar e excluir veículos</small></p>
+                <div class="col-12 col-lg-6">
                 </div>
             </div>
-            <p class="pequenas-palavras">Este programa faz parte da atividade final da disciplina de <span class="label-strong">Programação Orientada a Objetos em PHP</span>.
-                 Ele apresenta uma simulação dinâmica de uma fábrica de veículos, permitindo criar, gerenciar e vender diferentes tipos de veículos</p>
-        </div>
-
-        <div class="container-direito">
         </div>
     </div>
 </body>
