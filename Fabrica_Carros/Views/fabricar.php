265408546f49e5db77d4afd8248f4d8fea0edf64
niveabeh <niveasouza86@gmail.com>
2026-03-17 16:03:02 -0300

    modificando pag para ficar com o layout do projeto

--- Fabrica_Carros/Views/fabricar.php
+++ Fabrica_Carros/Views/fabricar.php
@@ -1,60 +1,64 @@
-
-?>
-
-
-
-
 <!DOCTYPE html>
 <html lang="pt-br">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/crud-fabrica-carros-php/mvc/views/css/style.css">
+    <link rel="stylesheet" href="/crud-fabrica-carros-php/Fabrica_Carros/Views/assets/css/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" defer></script>
-    <script src="/crud-fabrica-carros-php/mvc/views/js/main.js" defer> </script>
-    <title>Fabricar</title>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous" defer></script>
+    <title>Home</title>
 </head>
 
 <body>
-    <div class="container-pai">
-        <div class="container-esquerdo">
-
-            <div class="fotos-integrantes">
-                <img src="../assets/img/emilly.png" class="fotos-inte">
-                <img src="../assets/img/nivea.jpg" class="fotos-inte">
-                <img src="../assets/img/ana.png" class="fotos-inte">
-            </div>
-
-            <div class="titulos">
-                <h2 class="subtitulo-principal">monte o seu veículo</h2>
-                <h1 class="texto-h1">Fábrica de Veículos</h1>
-            </div>
-
-            <div class="caixa-card">
-                <h3>Informe os dados dos veículos</h3>
-
-                <form action="index.php?a=fabricando" method="POST">
-
-                    <div class="bloco-veiculo">
-                        <label>Modelo </label>
-                        <input type="text" name="modelo" id="modelo" required>
-                        <label>Cor</label>
-                        <input type="text" name="cor" id="cor" required>
+    <div class="container">
+        <div class="row min-vh-100 d-flex  align-items-center justify-content-start">
+            <div class="col-12 col-lg-6">
+                <!-- fotos -->
+                <div class="container mb-4">
+                    <div class="row">
+                        <div class="col-2">
+                            <img src="/crud-fabrica-carros-php/Fabrica_Carros/Views/assets/img/nivea.jpg" class="img-fluid w-25 rounded-circle shadow border border-white " alt="Imagem perfil Nívea">
+                            <img src="/crud-fabrica-carros-php/Fabrica_Carros/Views/assets/img/emilly.png" class="img-fluid w-25 rounded-circle shadow border border-white " alt="Imagem perfil Nívea">
+                        </div>
                     </div>
-
-                    <div class="caixa-btn">
-                        <button class="btn-primeiro" type="submit">Confirmar</button>
-                        <a href="index.php?a=home" class="btn-segundo">Voltar</a>
-                    </div>
-                </form>
+                </div>
+                <div class="mb-2">
+                    <h2 class="h2 text-secondary">Home</h2>
+                    <h1 class="text-roxo h1 fw-bold  ">Fábrica de Veículos</h1>
+                </div>
+                <div class="rounded-4 shadow bg-light p-2 mt-4 ">
+                    <h3 class="text-center text-secondary">Informe os dados dos veículos</h3>
+                    <form action="index.php?a=fabricando" method="POST" class="p-3">
+                        <div class="row g-3">
+                            <div class="col-12">
+                                <label for="modelo" class="form-label">Modelo</label>
+                                <input type="text" name="modelo" id="modelo"
+                                    class="form-control"
+                                    required>
+                            </div>
+                            <div class="col-12">
+                                <label for="cor" class="form-label">Cor</label>
+                                <input type="text" name="cor" id="cor"
+                                    class="form-control"
+                                    required>
+                            </div>
+                        </div>
+                        <div class="d-flex gap-2 mt-3 d-flex flex-column">
+                            <button type="submit" class="btn btn-primeiro">
+                                Confirmar
+                            </button>
+                            <a href="index.php?a=home" class="btn btn-outline-primeiro">
+                                Voltar
+                            </a>
+                        </div>
+                    </form>
+                </div>
+                <p class="text-secondary mt-4"><small> Este programa faz parte da atividade final da disciplina de <span class="text-roxo fw-semibold">Programação Orientada a Objetos em PHP</span>. O projeto apresenta uma simulação dinâmica de uma fábrica de veículos, permitindo criar, listar, editar e excluir veículos</small></p>
+                <div class="col-12 col-lg-6">
+                </div>
             </div>
-
         </div>
-
-        <div class="container-direito"></div>
-
     </div>
 </body>
 
