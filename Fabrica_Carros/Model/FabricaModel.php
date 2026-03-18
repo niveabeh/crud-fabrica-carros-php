c48e27c769f9ae56f04ddedc26329f1d7667dcfa
niveabeh <niveasouza86@gmail.com>
2026-03-18 10:30:00 -0300

    apenas algumas modificações para otimizar

--- Fabrica_Carros/Model/FabricaModel.php
+++ Fabrica_Carros/Model/FabricaModel.php
@@ -7,64 +7,47 @@ class FabricaModel extends Connect
     private $table = "veiculos";
 
 
-
     //meu delete
     public function deletarVeiculo($id)
     {
-        $sql = "DELETE FROM {$this->table} WHERE id = :id";
-        $stmt = $this->connection->prepare($sql);
-        $stmt->bindParam(':id', $id);
-        return $stmt->execute();
+        $sql = "DELETE FROM {$this->table} WHERE id = ?";
+        $consulta = $this->connection->prepare($sql);
+        return $consulta->execute([$id]);
     }
 
     //meu create
     public function fabricarVeiculos($modelo, $cor)
     {
-        $sql = "INSERT INTO veiculos (modelo, cor)
-            VALUES (:modelo, :cor)";
-
-        $stmt = $this->connection->prepare($sql);
-
-        return $stmt->execute([
-            ':modelo' => $modelo,
-            ':cor'    => $cor
-        ]);
+        $sql = "INSERT INTO {$this->table} (modelo, cor) VALUES (?, ?)";
+        $consulta = $this->connection->prepare($sql);
+        return $consulta->execute([$modelo,$cor]);
     }
 
     //meu sql para exibir todos os dados
     public function listar()
     {
         $sql = "SELECT * FROM {$this->table}";
-        $stmt = $this->connection->prepare($sql);
-        $stmt->execute();
+        $consulta = $this->connection->prepare($sql);
+        $consulta->execute();
 
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        return $consulta->fetchAll(PDO::FETCH_ASSOC);
     }
 
     //para editar algum veículo
     public function editarVeiculo($id, $modelo, $cor)
     {
-        $sql = "UPDATE $this->table
-                SET modelo = :modelo, cor = :cor 
-                WHERE id = :id";
-
-        $stmt = $this->connection->prepare($sql);
-        $stmt->bindParam(':id', $id);
-        $stmt->bindParam(':modelo', $modelo);
-        $stmt->bindParam(':cor', $cor);
-
-        return $stmt->execute();
+        $sql = "UPDATE {$this->table} SET modelo = ?, cor = ? WHERE id = ?";
+        $consulta = $this->connection->prepare($sql);
+        return $consulta->execute([$modelo, $cor, $id]);
     }
 
 
     //buscar
     public function buscarPorId($id)
     {
-        $sql = "SELECT * FROM {$this->table} WHERE id = :id LIMIT 1";
-        $stmt = $this->connection->prepare($sql);
-        $stmt->bindParam(':id', $id);
-        $stmt->execute();
-
-        return $stmt->fetch(PDO::FETCH_ASSOC);
+        $sql = "SELECT * FROM {$this->table} WHERE id = ?";
+        $consulta = $this->connection->prepare($sql);
+        $consulta->execute([$id]);
+        return $consulta->fetch(PDO::FETCH_ASSOC);
     }
 }
